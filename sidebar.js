db9ea8755f373e9d8dc4a6ec5723698bfa0a759d
futpib <futpib@gmail.com>
2018-12-08 23:04:34 +0300

    Remove unused sidebar

--- sidebar.js
+++
@@ -1,9 +0,0 @@
-/* global window */
-
-const r = require('r-dom');
-
-const { render } = require('react-dom');
-
-const Root = () => r.div('asdfasdf');
-
-render(r(Root), window.document.getElementById('content'));
